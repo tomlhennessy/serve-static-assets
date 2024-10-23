9ee4992bb12d9f09fd5e933bb17fdd66179485a4
Tom Hennessy <tomlhennessy@outlook.com>
2024-10-23 10:22:04 +1000

    phase 1: serving a single asset

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require('http');
 const fs = require("fs");
 
+// read the index.html file and store it in a variable
+const indexFile = fs.readFileSync('./index.html', 'utf-8')
+
 const server = http.createServer((req, res) => {
-  // Your code here
+  // send the index.html file as the response to any request
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(indexFile);
+  res.end();
 });
 
-const port = 5000;
+const port = 4000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
